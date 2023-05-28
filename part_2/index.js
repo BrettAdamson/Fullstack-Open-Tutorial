a9e8b6a4ef983f1168451adc2ed8e899827778c1
BrettAdamson <brettmadamson@gmail.com>
2023-05-28 18:28:52 -0500

    added env files, and connected the backend to the database

--- part_2/index.js
+++ part_2/index.js
@@ -1,27 +1,21 @@
+require('dotenv').config({path:__dirname+'/../config.env'})
 const express = require('express')
 const app = express()
-app.use(express.json())
 const cors = require('cors')
+const Note = require('./models/note')
+const PORT = process.env.PORT
+app.use(express.json())
 
 app.use(cors())
 app.use(express.static('build'))
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
+
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+app.use(unknownEndpoint)
+
 const generateId = () => {
     const maxId = notes.length > 0
         ? Math.max(...notes.map(n => n.id))
@@ -31,42 +25,36 @@ const generateId = () => {
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+  
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
-
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId(),
-    }
-
-    notes = notes.concat(note)
-
-    response.json(note)
-})
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    })
+  
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
+  })
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
-})
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
+  })
 
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -75,7 +63,6 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
