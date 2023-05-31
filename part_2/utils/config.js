3d40f917474344a00b98a38527570b2c5a6276df
BrettAdamson <brettmadamson@gmail.com>
2023-05-31 17:29:25 -0500

    added test database

--- part_2/utils/config.js
+++ part_2/utils/config.js
@@ -1,8 +1,9 @@
 require('dotenv').config({ path: __dirname + '/../config.env' })
-// D:\Projects\React Projects\Fullstack-Open-Tutorial\part_2\utils\config.js
-console.log(process.env)
+
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
