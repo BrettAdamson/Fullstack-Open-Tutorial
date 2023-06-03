19b503941f7bf5db3c10f1e7eb82c3699d948339
BrettAdamson <brettmadamson@gmail.com>
2023-06-02 20:02:56 -0500

    testing of the backend complete for now

--- part_2/controllers/notes.js
+++ part_2/controllers/notes.js
@@ -6,16 +6,6 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes)
 })
 
-
-notesRouter.get('/:id', async (request, response) => {
-  const note = await Note.findById(request.params.id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
-
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
@@ -28,11 +18,19 @@ notesRouter.post('/', async (request, response) => {
   response.status(201).json(savedNote)
 })
 
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
 notesRouter.delete('/:id', async (request, response) => {
   await Note.findByIdAndRemove(request.params.id)
   response.status(204).end()
 })
-
 
 notesRouter.put('/:id', (request, response, next) => {
   const body = request.body
